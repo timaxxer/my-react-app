d4c7b423b7477ea3581127a5a09c12053139f1d4
Yanick Tremblay <yanick.tremblay@aero.bombardier.com>
2017-12-05 19:55:02 -0500

    Initial commit

---
+++ src/containers/app/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route, Link } from 'react-router-dom'
+import Home from '../home'
+import About from '../about'
+import withRoot from 'components/withRoot';
+
+const App = () => (
+  <div>
+    <header>
+      <Link to="/">Home</Link>
+      <Link to="/about-us">About</Link>
+    </header>
+
+    <main>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about-us" component={About} />
+    </main>
+  </div>
+)
+
+export default withRoot(App)
