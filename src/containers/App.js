232455ad66a805e23dae91ed8ad1cab3efd27492
Unknown <yt.account@icloud.com>
2018-02-14 21:50:28 -0500

    Draft Notification Center

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,6 +7,7 @@ import About from 'containers/About'
 import withRoot from 'components/withRoot'
 import { withStyles } from 'material-ui/styles'
 import AppBar from 'material-ui/AppBar'
+import Avatar from 'material-ui/Avatar'
 import Toolbar from 'material-ui/Toolbar'
 import Drawer from 'material-ui/Drawer'
 import Background from 'components/Background'
@@ -14,7 +15,7 @@ import Reboot from 'material-ui/Reboot'
 import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles'
 import Typography from 'material-ui/Typography'
 import ListSubheader from 'material-ui/List/ListSubheader'
-import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List'
+import List, { ListItem, ListItemAvatar, ListItemIcon, ListItemText } from 'material-ui/List'
 import Collapse from 'material-ui/transitions/Collapse'
 import InboxIcon from 'material-ui-icons/MoveToInbox'
 import DraftsIcon from 'material-ui-icons/Drafts'
@@ -24,7 +25,10 @@ import ExpandMore from 'material-ui-icons/ExpandMore'
 import StarBorder from 'material-ui-icons/StarBorder'
 import IconButton from 'material-ui/IconButton';
 import MenuIcon from 'material-ui-icons/Menu';
+import Divider from 'material-ui/Divider';
 import AccountCircle from 'material-ui-icons/AccountCircle';
+import Alarm from 'material-ui-icons/Alarm';
+import PowerSettingsNew from 'material-ui-icons/PowerSettingsNew';
 import Menu, { MenuItem } from 'material-ui/Menu';
 
 const theme = createMuiTheme({
@@ -79,14 +83,19 @@ class App extends Component {
   handleMenu = event => {
     this.setState({ anchorEl: event.currentTarget });
   };
+  handleNotifMenu = event => {
+    this.setState({ notifAnchorEl: event.currentTarget });
+  };
 
-  handleClose = () => {
-    this.setState({ anchorEl: null });
+  handleClose = (event) => {
+    console.log(event)
+    this.setState({ anchorEl: null, notifAnchorEl: null });
   };
   render() {
     const { classes } = this.props;
-    const { anchorEl } = this.state;
+    const { anchorEl, notifAnchorEl } = this.state;
     const open = Boolean(anchorEl);
+    const notifOpen = Boolean(notifAnchorEl);
 
     return (
       <MuiThemeProvider theme={theme}>
@@ -97,21 +106,49 @@ class App extends Component {
               <IconButton onClick={this.toggleDrawer} className={classes.menuButton} color="inherit" aria-label="Menu">
                 <MenuIcon />
               </IconButton>
-              <Typography variant="title" color="inherit">
+              <Typography variant="title" color="inherit" noWrap>
                 Workbooks
               </Typography>
               <div className={classes.grow} />
               <AppSearch />
               <IconButton
-                aria-owns={open ? 'menu-appbar' : null}
-                aria-haspopup="true"
+                onClick={this.handleNotifMenu}
+                color="inherit"
+              >
+                <Alarm />
+              </IconButton>
+              <IconButton
                 onClick={this.handleMenu}
                 color="inherit"
               >
                 <AccountCircle />
               </IconButton>
               <Menu
-                id="menu-appbar"
+                anchorEl={notifAnchorEl}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'right',
+                }}
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={notifOpen}
+                onClose={this.handleClose}
+              >
+                <ListSubheader>Notification</ListSubheader>
+                <Divider zIndex={-1} />
+                <ListItem onClick={this.handleClose}>
+                  <ListItemAvatar>
+                    <Avatar> YT </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="Single-line item"
+                    secondary={'Secondary text'}
+                  />
+                </ListItem>
+              </Menu>
+              <Menu
                 anchorEl={anchorEl}
                 anchorOrigin={{
                   vertical: 'bottom',
@@ -126,6 +163,13 @@ class App extends Component {
               >
                 <MenuItem dense onClick={this.handleClose}>Yanick Tremblay account</MenuItem>
                 <MenuItem dense onClick={this.handleClose}>My account</MenuItem>
+                <Divider />
+                <MenuItem dense onClick={this.handleClose}>
+                  <ListItemIcon>
+                    <PowerSettingsNew />
+                  </ListItemIcon>
+                  Logout
+                </MenuItem>
               </Menu>
             </Toolbar>
           </AppBar>
@@ -141,14 +185,12 @@ class App extends Component {
               component="nav"
               subheader={<ListSubheader component="div">Nested List Items</ListSubheader>}
             >
-              <Link to={'/'} >
-                <ListItem button onClick={this.toggleDrawer}>
-                  <ListItemIcon>
-                    <SendIcon />
-                  </ListItemIcon>
-                  <ListItemText inset primary="Home" />
-                </ListItem>
-              </Link>
+              <ListItem component={Link} to={'/'} button onClick={this.toggleDrawer}>
+                <ListItemIcon>
+                  <SendIcon />
+                </ListItemIcon>
+                <ListItemText inset primary="Home" />
+              </ListItem>
               <ListItem button>
                 <ListItemIcon>
                   <DraftsIcon />
